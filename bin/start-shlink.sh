7ceaa39c2e529c0b03b72d916538e562b5023c47
Jérémy Buget <jbuget@gmail.com>
2025-09-14 12:27:38 +0200

    Add debug logs

--- bin/start-shlink.sh
+++ bin/start-shlink.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Debug, echo every command
+if [[ -n "$BUILDPACK_DEBUG" ]]; then
+  set -x
+fi
+
+# Fail immediately on non-zero exit code.
+set -e
+# Fail immediately on non-zero exit code within a pipeline.
+set -o pipefail
+# Fail on undeclared variables.
+set -u
+
 echo -n "-----> Installing RR... "
 php $1/vendor/bin/rr get --no-interaction --location $1/bin/ && chmod +x $1/bin/rr
 ls -la $1/bin
