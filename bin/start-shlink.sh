0cea2f30e81ff3489490b114623769fa3da280cc
Jérémy Buget <jbuget@gmail.com>
2025-09-15 08:58:27 +0200

    Another try

--- bin/start-shlink.sh
+++ bin/start-shlink.sh
@@ -12,5 +12,11 @@ set -o pipefail
 # Fail on undeclared variables.
 set -u
 
+pwd
+ls -a
+
+echo "-----> Installing RR..."
+php ./vendor/bin/rr get --no-interaction --location bin/ && chmod +x bin/rr
+
 echo "-----> Starting Shlink..."
-exec "/app/bin/rr" serve -o ${HOST:-0.0.0.0}:${PORT:-8080}
+exec bash bin/rr serve -o ${HOST:-0.0.0.0}:${PORT:-8080}
